a16b760f31a061e78e121475d658224fdb935156
xuzhuohao <786725551@qq.com>
2019-09-18 16:08:55 +0800

    init application and add apollo

---
+++ short-url-service/src/main/java/cn/com/bluemoon/shorturl/ShortUrlServiceApplication.java
@@ -0,0 +1,17 @@
+package cn.com.bluemoon.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * application
+ *
+ * @author XuZhuohao
+ * @date 2019/9/18
+ */
+@SpringBootApplication
+public class ShortUrlServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlServiceApplication.class);
+    }
+}
