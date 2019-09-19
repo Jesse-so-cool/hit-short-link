16c019896d92d1459b49016a82a0ac100eeb016f
xuzhuohao <786725551@qq.com>
2019-09-19 13:15:18 +0800

    添加配置文件

---
+++ short-url-service/src/main/java/cn/com/bluemoon/shorturl/config/ShortUrlConfig.java
@@ -0,0 +1,16 @@
+package cn.com.bluemoon.shorturl.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author XuZhuohao
+ * @date 2019/9/19
+ */
+@Data
+@Configuration
+public class ShortUrlConfig {
+    @Value("${short.url.domain}")
+    private String domain;
+}
