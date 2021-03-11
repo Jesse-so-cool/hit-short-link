5256aa9097ea1900740dbb9080d6c3ebd03466a0
黄时捷 <huangshijie2@bluemoon.com.cn>
2021-03-11 13:43:39 +0800

    init

--- short-url-api/src/main/java/cn/com/bluemoon/shorturl/servcie/ResponseService.java
+++
@@ -1,9 +0,0 @@
-package cn.com.bluemoon.shorturl.servcie;
-
-import cn.com.bluemoon.shorturl.dto.ShortUrlDto;
-import com.bluemoon.pf.standard.bean.ResponseBean;
-
-public interface ResponseService {
-    ResponseBean longToShort(ShortUrlDto shortUrlDto);
-    ResponseBean shortToLong(String uri);
-}
--- short-url-service/src/main/resources/redis.lua
+++
@@ -1,8 +0,0 @@
-local data = redis.call('lrange', KEYS[1], ARGV[1],ARGV[2])
-local size = table.getn(data);
-if size == 0 then
-    return nil
-else
-    redis.call('ltrim', KEYS[1], size,-1)
-    return cjson.encode(data)
-end
